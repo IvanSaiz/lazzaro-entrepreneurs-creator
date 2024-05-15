0ad60aa6e3677c5aaab329ab28a8e973e31b1f62
Valentin Marchessi <vmarchessi@gmail.com>
2024-05-15 06:02:32 -0300

    Add fallback value

--- src/modules/auth/views/Onboarding/views/stepStyle/index.vue
+++ src/modules/auth/views/Onboarding/views/stepStyle/index.vue
@@ -104,7 +104,7 @@
         logo: uploadedImage,
         textColor: this.styleForm.textColor,
         buttonColor: this.styleForm.buttonColor,
-        template: this.styleForm.template
+        template: this.styleForm.template ?? "modern"
       });
 
       await this.setTemplateFeatures();
